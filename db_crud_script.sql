07e091334a2a37c558aa4965a538624780c07288
Lucas Bruce <lucasmatheusbruce@gmail.com>
2020-08-09 19:56:24 -0300

    crud v0.1

--- db_crud_script.sql
+++ db_crud_script.sql
@@ -40,3 +40,46 @@ select * from tbl_usuario;
 /*deletando o registro da tabela(CRUD)
 D -> delete -> delete*/
 delete from tbl_usuario where id_usuario = 3;
+
+create table if not exists tbl_cliente(
+  id_cliente int primary key auto_increment,
+  nome_cliente varchar(50) not null,
+  email_cliente varchar(50) 
+);
+
+create table if not exists tbl_tipo_logradouro(
+  id_tipo_logradouro int primary key auto_increment,
+  tipo_logradouro varchar(50)
+);
+
+create table if not exists tbl_endereco_cliente(
+  id_endereco_cliente int primary key auto_increment,
+  complemento varchar(50) not null,
+  nome_rua varchar(50) not null,
+  numero_rua varchar(50) not null,
+  cep char(9) not null,
+  id_cliente int not null,
+  id_tipo_logradouro int not null,
+  constraint fk_id_endereco_cliente_id_cliente foreign key (id_cliente)
+    references tbl_cliente (id_cliente),
+  constraint fk_id_endereco_cliente_id_tipo_logradouro foreign key (id_tipo_logradouro)
+    references tbl_tipo_logradouro(id_tipo_logradouro)
+);
+
+create table if not exists tbl_tipo_telefone(
+  id_tipo_telefone int primary key auto_increment,
+  tipo_telefone varchar(20) not null
+);
+
+create table if not exists tbl_telefone_cliente(
+  id_telefone_cliente int primary key auto_increment,
+  telefone_cliente varchar(15),
+  id_cliente int not null,
+  id_tipo_telefone int not null,
+  constraint fk_id_telefone_cliente_id_cliente foreign key (id_cliente)
+    references tbl_cliente (id_cliente),
+  constraint fk_id_telefone_cliente_id_tipo_telefone foreign key (id_tipo_telefone)
+    references tbl_tipo_telefone(id_tipo_telefone)
+);
+
+
